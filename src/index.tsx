f05701940b096583468223d69d49b18f57c36ac1
masapon06 <USERNAME@users.noreply.github.com>
2022-04-30 22:05:47 +0900

    first commit  add below - webpack - react - typescript

---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import React from "react"
+import { createRoot } from 'react-dom/client';
+
+const App = () => <div id='root'>Hello World</div> // develop new component
+
+const container = document.getElementById("app")
+const root = createRoot(container!)
+root.render(<App/>)
